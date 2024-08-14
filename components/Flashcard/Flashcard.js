c5abf9db3e48edfe93a27dc426f2bd1eb67b8c30
Laura Sliwa <l.sliwa@gmx.de>
2024-08-14 11:56:31 +0200

    Finish flip animation

--- components/Flashcard/Flashcard.js
+++ components/Flashcard/Flashcard.js
@@ -9,9 +9,9 @@ export default function Flashcard({ question, answer, collection }) {
   }
 
   return (
-    <StyledFlashcard onClick={handleShowAnswer}>
-      {!showAnswer && (
-        <>
+    <CardContainer onClick={handleShowAnswer}>
+      <StyledFlashcard $showAnswer={showAnswer}>
+        <CardFront>
           <CollectionTitle>{collection}</CollectionTitle>
           <Question>{question}</Question>
           <svg
@@ -23,11 +23,9 @@ export default function Flashcard({ question, answer, collection }) {
           >
             <path d="m700-300-57-56 84-84H120v-80h607l-83-84 57-56 179 180-180 180Z" />
           </svg>
-        </>
-      )}
-      {showAnswer && (
-        <>
-          <p>{answer}</p>
+        </CardFront>
+        <CardBack>
+          <Answer>{answer}</Answer>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -38,15 +36,44 @@ export default function Flashcard({ question, answer, collection }) {
           >
             <path d="m700-300-57-56 84-84H120v-80h607l-83-84 57-56 179 180-180 180Z" />
           </svg>
-        </>
-      )}
-    </StyledFlashcard>
+        </CardBack>
+      </StyledFlashcard>
+    </CardContainer>
   );
 }
 
+const CardContainer = styled.div`
+  perspective: 1000px;
+`;
+
 const StyledFlashcard = styled.section`
+  width: 19rem;
+  height: 13rem;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.6s;
+  transform: ${({ $showAnswer }) =>
+    $showAnswer ? "rotateY(180deg)" : "rotateY(0deg)"};
+  cursor: pointer;
+`;
+
+const CardFace = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
   border: 1px solid #000;
   padding: 10px;
+  border-radius: 10px;
+`;
+
+const CardFront = styled(CardFace)`
+  background-color: #fff;
+`;
+
+const CardBack = styled(CardFace)`
+  background-color: #f8f8f8;
+  transform: rotateY(180deg);
 `;
 
 const CollectionTitle = styled.p`
