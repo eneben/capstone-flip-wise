341d86722af3363d3670530dccec0a7fcecebce8
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-09-11 14:06:55 +0200

    add message in case there are no more collections

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,36 @@ export default function HomePage({
   return (
     <>
       <StyledHeadline>My flashcard collections</StyledHeadline>
-      <CollectionsWrapper>
-        {collections.map((collection) => {
-          return (
-            <Collection
-              key={collection.id}
-              collection={collection}
-              actionMode={actionMode}
-              getCorrectFlashcardsFromCollection={
-                getCorrectFlashcardsFromCollection
-              }
-              getIncorrectFlashcardsFromCollection={
-                getIncorrectFlashcardsFromCollection
-              }
-              flashcardSelection={flashcardSelection}
-              changeFlashcardSelection={changeFlashcardSelection}
-              handleDeleteCollection={handleDeleteCollection}
-            />
-          );
-        })}
-      </CollectionsWrapper>
+
+      {collections.length > 0 && (
+        <CollectionsWrapper>
+          {collections.map((collection) => {
+            return (
+              <Collection
+                key={collection.id}
+                collection={collection}
+                actionMode={actionMode}
+                getCorrectFlashcardsFromCollection={
+                  getCorrectFlashcardsFromCollection
+                }
+                getIncorrectFlashcardsFromCollection={
+                  getIncorrectFlashcardsFromCollection
+                }
+                flashcardSelection={flashcardSelection}
+                changeFlashcardSelection={changeFlashcardSelection}
+                handleDeleteCollection={handleDeleteCollection}
+              />
+            );
+          })}
+        </CollectionsWrapper>
+      )}
+
+      {collections.length === 0 && (
+        <StyledMessage>
+          All collections and the flashcards they contained have been deleted.
+          Add new flashcards to create new collections.
+        </StyledMessage>
+      )}
     </>
   );
 }
@@ -49,4 +59,10 @@ const StyledHeadline = styled.h2`
   text-align: center;
   padding: 35px 0 30px 0;
   font-size: 1.7rem;
+`;
+
+const StyledMessage = styled.p`
+  text-align: center;
+  font-size: 1rem;
+  padding: 40px 20px;
 `;
