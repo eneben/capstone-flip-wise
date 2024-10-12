0321684ee8d5ac6cf1e60f9f6eeb2a7c624bf0be
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-10-12 17:40:29 +0200

    add dummy user credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,42 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
-console.log("wird das aufgerufen?");
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      authorization: { params: { scope: "read:user" } },
-    }),
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "username",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials) {
+            if (
+              credentials.username === "test-user" &&
+              credentials.password === "katze123"
+            ) {
+              return {
+                name: "John Doe",
+                email: "test@example.com",
+                id: "a1b2c3d4",
+              };
+            } else {
+              return null;
+            }
+          },
+        })
+      : GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+          authorization: { params: { scope: "read:user" } },
+        }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
