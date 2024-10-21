98d9cf5527e8c4d59e56ce8762b1e6173d00df61
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-10-21 23:53:55 +0200

    delete collectionsarray from users schema

--- db/models/User.js
+++ db/models/User.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import "./Collection";
+// import "./Collection";
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   providerId: { type: String, required: true },
-  usersCollections: { type: [Schema.Types.ObjectId], ref: "Collection" },
+  // usersCollections: { type: [Schema.Types.ObjectId], ref: "Collection" },
 });
 
 userSchema.index({ providerId: 1 }, { unique: true });
