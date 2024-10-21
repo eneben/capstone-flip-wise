070eea4b3dd4145fadc8e7be05f60c82b014c2d8
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-10-21 21:43:42 +0200

    initial commit, create schemas

--- db/models/Collection.js
+++ db/models/Collection.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import User from "./User";
 
 const { Schema } = mongoose;
 
 const collectionSchema = new Schema({
   title: { type: String, required: true },
   color: { type: String, required: true },
+  ownerId: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 const Collection =
---
+++ db/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import Collection from "./Collection";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  providerId: { type: String, required: true },
+  usersCollections: { type: [Schema.Types.ObjectId], ref: "Collection" },
+});
+
+userSchema.index({ providerId: 1 }, { unique: true });
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
